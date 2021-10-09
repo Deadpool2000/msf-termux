d762c611123f9bdf6de749129e90fa5d81d95c1f
Deadpool2000 <d2kyt@protonmail.com>
2021-10-09 19:58:13 +0530

    Bug fix

--- install.sh
+++ install.sh
@@ -9,22 +9,30 @@
 
 echo ">>>>>    Script by Deadpool2000     <<<<<\n"
 ver=6.1.9
+path="/data/data/com.termux/files/home/"
 rm $PREFIX/bin/msfconsole && rm $PREFIX/bin/msfvenom > /dev/null
 rm -rf $HOME/metasploit-framework > /dev/null
-echo "\nInstalling required packages........\n"
+echo
+echo "Installing required packages........"
+echo
 apt update && apt upgrade
 apt install -y ruby wget apr apr-util libiconv zlib autoconf bison clang coreutils curl findutils git libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config make libgrpc termux-tools ncurses-utils ncurses tar termux-elf-cleaner unzip zip
-echo"\n Downloading latest metasploit.......\n"
+echo
+echo "Downloading latest metasploit......."
+echo
 wget -O msf.tar.gz https://github.com/rapid7/metasploit-framework/archive/$ver.tar.gz
-mv msf.tar.gz $HOME
-tar -xvf $HOME/msf.tar.gz
-mv $HOME/metasploit-framework-$ver $HOME/metasploit-framework 
-rm $HOME/msf.tar.gz
-echo "\nInstalling gems.........\n"
+mv msf.tar.gz $path
+tar -xvf $path/msf.tar.gz
+mv $path/metasploit-framework-$ver $path/metasploit-framework 
+rm $path/msf.tar.gz
+echo
+echo "Installing gems........."
+echo
 gem install --no-document --verbose rubygems-update 
 update_rubygems
 gem install bundler && bundle config build.nokogiri --use-system-libraries
-cd $HOME/metasploit-framework && bundle install
+cd $path/metasploit-framework && bundle install
 cp assets/msfconsole $PREFIX/bin/ && cp assets/msfvenom $PREFIX/bin/
 
-echo "\nNow just use commands 'msfconsole' or 'msfvenom' to run metasploit.\n"
+echo
+echo "Now just use commands 'msfconsole' or 'msfvenom' to run metasploit."
